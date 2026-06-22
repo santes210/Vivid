79fa250f01eedbf943b01c6d8e600cb7a1dbc33e
santes210 <poncho2010santes@gmail.com>
2026-06-21 20:44:01 -0600

    v 0.5.5

--- vivid-app/app/src/main/java/com/vivid/app/presentation/feed/FeedScreen.kt
+++ vivid-app/app/src/main/java/com/vivid/app/presentation/feed/FeedScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
