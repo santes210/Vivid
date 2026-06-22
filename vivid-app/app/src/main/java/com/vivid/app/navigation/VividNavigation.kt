0abf50614290f6f95b4d338634dc64571de9fbd6
santes210 <poncho2010santes@gmail.com>
2026-06-21 23:45:44 -0600

    v 0.6.1: Fix, Auth

--- vivid-app/app/src/main/java/com/vivid/app/navigation/VividNavigation.kt
+++ vivid-app/app/src/main/java/com/vivid/app/navigation/VividNavigation.kt
@@ -12,6 +12,7 @@ import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
